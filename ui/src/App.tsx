78829285c4e9067558a6dcdf44530840f5144229
misebox <misebox@gmail.com>
2025-09-06 21:53:22 +0900

    Add route change logging helper (commented out)

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,18 +1,36 @@
-import { Component } from 'solid-js'
-import { Router, Route, Navigate } from '@solidjs/router'
+import { Component, createEffect } from 'solid-js'
+import { Router, Route, Navigate, useLocation } from '@solidjs/router'
 import Home from './pages/Home'
 import Project from './pages/Project'
 import TableDetail from './pages/TableDetail'
 
+const withRouteLogger = (Component: any) => {
+  return (props: any) => {
+    const location = useLocation()
+    
+    createEffect(() => {
+      // console.debug('URL changed:', {
+      //   pathname: location.pathname,
+      //   search: location.search,
+      //   hash: location.hash,
+      //   fullUrl: location.pathname + location.search + location.hash,
+      //   timestamp: new Date().toISOString()
+      // })
+    })
+    
+    return <Component {...props} />
+  }
+}
+
 const App: Component = () => {
   return (
     <Router>
-      <Route path="/" component={Home} />
-      <Route path="/projects/:projectId" component={Project} />
-      <Route path="/projects/:projectId/tables/:tableId" component={TableDetail} />
-      <Route path="/projects/:projectId/views/:viewId" component={Project} />
-      <Route path="/projects/:projectId/functions/:functionId" component={Project} />
-      <Route path="/projects/:projectId/layouts/:layoutId" component={Project} />
+      <Route path="/" component={withRouteLogger(Home)} />
+      <Route path="/projects/:projectId" component={withRouteLogger(Project)} />
+      <Route path="/projects/:projectId/tables/:tableId" component={withRouteLogger(TableDetail)} />
+      <Route path="/projects/:projectId/views/:viewId" component={withRouteLogger(Project)} />
+      <Route path="/projects/:projectId/functions/:functionId" component={withRouteLogger(Project)} />
+      <Route path="/projects/:projectId/layouts/:layoutId" component={withRouteLogger(Project)} />
       <Route path="*" component={() => <Navigate href="/" />} />
     </Router>
   )
